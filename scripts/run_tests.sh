a4c45742358f46eb7fc2d176251208c15ddb631a
aoantov <a.o.antov@gmail.com>
2026-09-04 21:12:47 +0300

    change condition on test script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
-if [ ! -f ../plenary.nvim ]; then
+if [ ! -d ../plenary.nvim ]; then
   git clone --depth 1 https://github.com/nvim-lua/plenary.nvim ../plenary.nvim
 fi
 nvim --headless --noplugin -u scripts/minimal.vim -c "PlenaryBustedDirectory tests/ {minimal_init = 'scripts/minimal.vim'}"
